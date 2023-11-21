44ea8caa63eb71e37530fe5157216336386cd1ac
jeunh <gkwldms1218@gmail.com>
2023-11-21 23:22:18 +0900

    컴포넌트 구현

---
+++ src/components/TodoList.vue
@@ -0,0 +1,22 @@
+<template>
+  <ul>
+    <li v-for="(item, index) in items" :key="index">{{ item }}</li>
+  </ul>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+  export default {
+    setup() {
+      // data
+      const items = ref(['애플', '삼성', '인프런']);
+
+      return { items }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
