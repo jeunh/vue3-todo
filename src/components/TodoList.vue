484fbc4c516dfd464fa6e5278a71b9205a20c942
jeunh <gkwldms1218@gmail.com>
2023-11-23 22:38:45 +0900

    watch API

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -8,12 +8,19 @@
 </template>
 
 <script>
+// import { watch } from 'vue';
+
   export default {
     props: ['todoItems'],
     setup(props, context) {
       function removeTodo(item, index) {
         context.emit('remove', item, index);
       }
+
+      // 최대한 사용하지 않는 것이 좋음 - 데이터 변경 시 추적이 어려울 수 있고, 관리가 어려울 수 있음
+      // watch(props.todoItems, (newValue) => {
+      //   console.log(newValue);
+      // })
 
       return { removeTodo }
     }
